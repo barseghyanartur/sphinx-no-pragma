f527c2906c8e1043e7a914f7e88f41706bd1c7d0
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-12-18 21:50:20 +0100

    Initial

---
+++ docs/_static/example_1.py
@@ -0,0 +1,8 @@
+from typing import Any, Optional
+
+
+def my_func(arg1: Optional[Any] = None) -> int:
+    # This is a very long line that should normally fail but we want it to be present as is
+    a = arg1 or None or None or None or None or None or None or None or None or None or None  # noqa
+
+    return "0"  # type: ignore
